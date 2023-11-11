9f1b889763a59181a5d8e12b75d60be56c6e3f07
nicolassoam <nsm4456@gmail.com>
2023-11-11 14:28:59 -0300

    corrige regra

--- Rules.java
+++ Rules.java
@@ -71,10 +71,16 @@ public class Rules {
         _Buckets[1].setAmount(buckets[1].getAmount());
         _Buckets[1].setLimit(buckets[1].getLimit());
         
-        int maxAmount = _Buckets[1].getLimit() - _Buckets[1].getAmount();
+        int amount = _Buckets[0].getAmount() + _Buckets[1].getAmount();
+        if(_Buckets[0].getAmount() + _Buckets[1].getAmount() > _Buckets[1].getLimit()) {
+            _Buckets[1].setAmount(3);
+            _Buckets[0].setAmount(amount - _Buckets[1].getLimit());
+        }
+        else {
+            _Buckets[1].setAmount(amount);
+            _Buckets[0].setAmount(0);
+        }
         
-        _Buckets[1].setAmount(maxAmount + _Buckets[1].getAmount());
-        _Buckets[0].setAmount(_Buckets[0].getAmount() - maxAmount);
         return _Buckets;
     }
 
