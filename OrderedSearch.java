523eb8a15bf97d928ee8bdb70aa829a4a76e837c
IAmTheMage <gustavogoncalvessilva2003@outlook.com>
2023-11-30 15:30:59 -0300

    Fix shifted water bug

--- OrderedSearch.java
+++ OrderedSearch.java
@@ -16,7 +16,7 @@ public class OrderedSearch {
     private int getShiftedWater(Bucket[] a0, Bucket[] a2, int rule) {
         int shifted1 = Math.abs(a0[0].getAmount() - a2[0].getAmount());
         int shifted2 = Math.abs(a0[1].getAmount() - a2[1].getAmount());
-        return rule == 1 || rule == 2 ? (shifted1 + shifted2) / 2 : shifted1 + shifted2 ;
+        return rule == 5 || rule == 6 ? (shifted1 + shifted2) / 2 : shifted1 + shifted2 ;
     }
 
     public Node exec(Node initialCondition, List<Integer> rulesOrder) throws InterruptedException {
